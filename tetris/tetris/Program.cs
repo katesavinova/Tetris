045e2061820f7a837f7d0daca79db08af61cf563
katesavinova <63250525+katesavinova@users.noreply.github.com>
2020-04-24 16:41:27 +0400

    Добавление точек, фукция по их выводу

--- tetris/tetris/Program.cs
+++ tetris/tetris/Program.cs
@@ -6,7 +6,20 @@ namespace tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(35, 30);//изменение размера окна для игры
+            Console.SetBufferSize(35, 30);//Убрать полоски прокрутки
+
+            int x1 = 2, y1 = 3, x2 = 2, y2 = 9;
+            char c1 = '*', c2='+';
+            Drow(x1,y1,c1);
+            Drow(x2, y2, c2);
+
+            Console.ReadLine();
+            static void Drow(int x, int y, char c)//функция,выводящая один символ
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(c);
+            }
         }
     }
 }
